0b15d273aa384287d4d7665e2d48d22b094c5714
nanaf <nana.feiqrishvili2014@gmail.com>
2021-08-07 03:51:38 +0400

    checked email and password validation

--- app/src/main/java/com/example/todolist/RegisterActivity.kt
+++ app/src/main/java/com/example/todolist/RegisterActivity.kt
@@ -1,16 +1,8 @@
 package com.example.todolist
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Patterns
-import android.widget.Toast
 import com.example.todolist.databinding.ActivityRegisterBinding
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 class RegisterActivity : AppCompatActivity() {
 
